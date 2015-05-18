97c8ec998b44323915a9c304d6495db6d51cb3f1
Alexander Belov <git@belov.by>
2015-05-18 21:09:43 +0300

    Use ER diagrams

--- js/app.js
+++ js/app.js
@@ -1,27 +1,65 @@
-$(function () {
-    var graph = new joint.dia.Graph;
+var graph = new joint.dia.Graph;
 
-    var paper = new joint.dia.Paper({
-        el: $('#myholder'),
-        width: 800,
-        height: 600,
-        model: graph,
-        gridSize: 1
-    });
-
-    var rect = new joint.shapes.basic.Rect({
-        position: { x: 100, y: 30 },
-        size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'ars box', fill: 'white' } }
-    });
-
-    var rect2 = rect.clone();
-    rect2.translate(300);
-
-    var link = new joint.dia.Link({
-        source: { id: rect.id },
-        target: { id: rect2.id }
-    });
-
-    graph.addCells([rect, rect2, link]);
+var paper = new joint.dia.Paper({
+  el: $('#paper'),
+  width: 800,
+  height: 600,
+  gridSize: 5,
+  model: graph,
+  //embeddingMode: true
 });
+
+var erd = joint.shapes.erd;
+
+var element = function(elm, x, y, label) {
+  var cell = new elm({ position: { x: x, y: y }, attrs: { text: { text: label }}});
+  graph.addCell(cell);
+  return cell;
+};
+
+var link = function(elm1, elm2) {
+  var myLink = new erd.Line({ source: { id: elm1.id }, target: { id: elm2.id }});
+  graph.addCell(myLink);
+  return myLink;
+};
+
+var dependence = function(elm1, elm2) {
+  var myLink = new erd.Dependence({ source: { id: elm1.id }, target: { id: elm2.id }});
+  graph.addCell(myLink);
+  return myLink;
+}
+
+var influence = function(elm1, elm2) {
+  var myLink = new erd.Influence({ source: { id: elm1.id }, target: { id: elm2.id }});
+  graph.addCell(myLink);
+  return myLink;
+}
+
+var employee = element(erd.Entity, 100, 200, "Employee");
+var salesman = element(erd.Entity, 100, 400, "Salesman");
+var wage = element(erd.WeakEntity, 530, 200, "Wage");
+var paid = element(erd.IdentifyingRelationship, 350, 190, "gets paid");
+var isa = element(erd.Rate, 125, 300, "Rate");
+var number = element(erd.Key, 0, 90, "number");
+var nameEl = element(erd.Normal, 75, 30, "name");
+var skills = element(erd.Multivalued, 150, 90, "skills");
+var amount = element(erd.Derived, 440, 80, "amount");
+var date = element(erd.Normal, 590, 80, "date");
+var plate = element(erd.Key, 405, 500, "plate");
+var car = element(erd.Entity, 430, 400, "Company car");
+var uses = element(erd.Relationship, 300, 390, "uses");
+
+employee.embed(number);
+
+link(employee, paid).cardinality('1');
+dependence(number, employee);
+influence(nameEl, employee);
+link(employee, skills);
+link(employee, isa);
+link(isa, salesman);
+link(salesman, uses).cardinality('0..1');
+link(car, uses).cardinality('1..1');
+link(car, plate);
+link(wage, paid).cardinality('N');
+link(wage, amount);
+link(wage, date);
