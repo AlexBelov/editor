e828bf320e20cb0e56dc726a52afcf86d4833ce8
ars <arsemchik@gmail.com>
2015-06-09 20:57:22 +0300

    element clone on click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 $(function () {
+    // ============================================================
+    //BASE PAPER
+    // ============================================================
     var paper_element =  $('#paper');
+
     paper_element.css('height', $( window ).height());
+
     var graph = new joint.dia.Graph;
 
     var paper = new joint.dia.Paper({
@@ -10,26 +15,40 @@ $(function () {
         model: graph,
         gridSize: 1
     });
+    // ============================================================
+
+
+    // ============================================================
+    // MENU
+    // ============================================================
+    var menu_element = $('#menu');
+    var graph_menu = new joint.dia.Graph;
+
+
+    var menu = new joint.dia.Paper({
+        el: menu_element,
+        width: ($('.panel-body.menu').width() - 5),
+        height: $( window ).height(),
+        model: graph_menu,
+        gridSize: 1,
+        interactive: false
+    });
 
     var rect = new joint.shapes.basic.Rect({
-        position: { x: 100, y: 30 },
+        position: { x: 10, y: 10},
         size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'ars box', fill: 'white' } }
+        attrs: { rect: { fill: 'blue' }, text: { text: 'ars box', fill: 'white' } },
+        interactive: false
     });
 
-    var rect2 = rect.clone();
-    rect2.translate(300);
+    graph_menu.addCells([rect]);
 
-    var link = new joint.dia.Link({
-        source: { id: rect.id },
-        target: { id: rect2.id }
-    });
-
-    graph.addCells([rect, rect2, link]);
-
-    //paper.on('cell:pointerdown',
-    //    function(cellView, evt, x, y) {
-    //        alert('cell view ' + cellView.model.type + ' was clicked');
-    //    }
-    //);
+    menu.on('cell:pointerdown',
+        function(cellView, evt, x, y) {
+            var rect2 = cellView.model.clone();
+            rect2.translate(400);
+            graph.addCells([rect2]);
+        }
+    );
+    // ============================================================
 });
