5eff88dd312949f7cb4bb3afad43822512062023
ars <arsemchik@gmail.com>
2015-06-27 17:19:23 +0300

    shapes

---
+++ js/custom_shape.js
@@ -0,0 +1,60 @@
+// Create a custom element.
+// ------------------------
+joint.shapes.html = {};
+joint.shapes.html.Rectangle = joint.shapes.basic.Rect.extend({
+    defaults: joint.util.deepSupplement({
+        type: 'html.Rectangle',
+        attrs: {
+            rect: {stroke: 'none', 'fill-opacity': 0}
+        }
+    }, joint.shapes.basic.Rect.prototype.defaults)
+});
+
+
+// Create a custom view for that element that displays an HTML div above it.
+// -------------------------------------------------------------------------
+joint.shapes.html.RectangleView = joint.dia.ElementView.extend({
+
+    template: [
+        '<div class="html-element">',
+        '<button class="delete">x</button>',
+        '<lable>some text</lable>'
+    ].join(''),
+
+    initialize: function () {
+        _.bindAll(this, 'updateBox');
+        joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+
+        this.$box = $(_.template(this.template)());
+
+        this.$box.find('.delete').on('click', _.bind(this.model.remove, this.model));
+        // Update the box position whenever the underlying model changes.
+        this.model.on('change', this.updateBox, this);
+        // Remove the box when the model gets removed from the graph.
+        this.model.on('remove', this.removeBox, this);
+    },
+    render: function () {
+        joint.dia.ElementView.prototype.render.apply(this, arguments);
+        this.paper.$el.prepend(this.$box);
+        this.updateBox();
+        return this;
+    },
+    updateBox: function () {
+        // Set the position and dimension of the box so that it covers the JointJS element.
+        var bbox = this.model.getBBox();
+
+        // Example of updating the HTML with a data stored in the cell model.
+        this.$box.css({
+            width: bbox.width,
+            height: bbox.height,
+            left: bbox.x,
+            top: bbox.y,
+            transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)'
+        });
+    },
+    removeBox: function (evt) {
+        if (menu_element.find(this)) {
+            this.$box.remove();
+        }
+    }
+});
